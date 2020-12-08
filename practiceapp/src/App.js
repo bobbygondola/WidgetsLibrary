f249b09b1a06182bd52e0e87d5b40ac945f958c4
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-12-08 14:23:03 -0500

    accordion/finished with state

--- practiceapp/src/App.js
+++ practiceapp/src/App.js
@@ -1,14 +1,27 @@
 import React, {useState} from 'react'
 import './App.css';
+import Accordion from './components/Accordion'
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front-end JavaScript framework."
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite front-end JavaScript framework for creating resuable UI components."
+  },
+  {
+    title: "How do you use React?",
+    content: "React is used by creating UI components to complete web applications."
+  }
+]
 
 function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        
-
-      </header>
+      <Accordion items={items}/>
     </div>
   );
 }
